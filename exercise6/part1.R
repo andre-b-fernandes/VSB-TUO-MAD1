b8649ea6fd9c10ffb1d0f1c90c2f6a81678baf4c
Fernando Fernandes <fernandoandre49@gmail.com>
2019-11-18 15:16:40 +0100

    new seminar done

---
+++ exercise6/part1.R
@@ -0,0 +1,89 @@
+if (!require("igraph")) 
+  install.packages("igraph")
+library(igraph) # verze 1.0.1
+
+df <- read.csv("KarateClub.csv", header = F, sep = ';', dec = ',')
+
+g1 <- graph_from_data_frame(d = df, directed = F)
+
+# Diameter
+diam <- get_diameter(g1, directed=F)
+diam_as_vector <- as.vector(diam)
+
+print("Diameter: ")
+diam
+
+# Floyd Warshall Algorithm 
+floyd <- function(dataFrame){
+  adjecency_matrix <- matrix(data = Inf , nrow = max(df[1]), ncol = max(df[1]))
+  for (row in 1:nrow(dataFrame)) {
+     node1 <- dataFrame[row,1]
+     node2 <- dataFrame[row,2]
+     adjecency_matrix[node1,node2] <- 1
+  }
+
+  for(index in 1:nrow(adjecency_matrix)){
+    adjecency_matrix[index,index] <- 0
+  }
+  
+  for (k in 1:nrow(adjecency_matrix)) {
+     for (i in 1:nrow(adjecency_matrix)) {
+        for (j in 1:nrow(adjecency_matrix)) {
+            dist1 <- adjecency_matrix[i,j]
+            dist2 <- adjecency_matrix[i,k]
+            dist3 <- adjecency_matrix[k,j]
+            if (dist1 > dist2 + dist3) {
+               adjecency_matrix[i,j] <- dist2 + dist3
+            }
+        }
+     }
+  }
+  return(adjecency_matrix)
+}
+
+min_distance_between_nodes = floyd(df)
+
+print("Mininum distance between all pair of vertexes: ")
+min_distance_between_nodes
+
+# Mean distance
+
+m_distance_graph = mean_distance(g1)
+
+print("Mean distance of the Graph: ")
+m_distance_graph
+
+mean_distance_for_each_vertex <- function(min_adj_matrix){
+    mean_distances = c(1:nrow(min_adj_matrix))
+    for (k in 1:nrow(min_adj_matrix)) {
+        row = min_adj_matrix[k,]
+        filtered_row = row[!is.na(row) & !is.infinite(row)]
+        mean_distances[k] <- mean(filtered_row)
+    }
+    return(mean_distances)
+}
+
+mean_distances = mean_distance_for_each_vertex(min_distance_between_nodes)
+
+print("Mean distance of each vertex: ")
+mean_distances
+
+# Frequency of distances
+
+frequency_table = table(min_distance_between_nodes)
+
+df_frequency_table <- as.data.frame(frequency_table)
+
+print("Frequency Table: ")
+df_frequency_table
+
+calculate_data_for_histogram <- function(df_freq){
+    data = c()
+    for (k in 1:nrow(df_freq)) {
+        data <- c(data, as.character(rep(df_freq[k,1], df_freq[k,2]))) 
+    }
+    return(as.numeric(data))
+}
+
+data <- calculate_data_for_histogram(df_frequency_table)
+hist(data, col="red", border="gold", main="Frequency of distances", xlab="Distances")
---
+++ exercise6/part2.R
@@ -0,0 +1,17 @@
+if (!require("igraph")) 
+  install.packages("igraph")
+library(igraph) # verze 1.0.1
+
+df <- read.csv("KarateClub.csv", header = F, sep = ';', dec = ',')
+
+g1 <- graph_from_data_frame(d = df, directed = F)
+
+#Local
+local_clustering_coefficients <- transitivity(g1, type = "local")
+print("Local clustering coefficients: ")
+local_clustering_coefficients
+
+#Global
+print("Graph transitivity: ")
+average_transivity <- transitivity(g1, type = "average")
+average_transivity
