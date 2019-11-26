aeb5ea8b626460a4dd9e1ffcd5ff7df623dfc70f
Fernando Fernandes <fernandoandre49@gmail.com>
2019-11-26 16:06:15 +0100

    k-means with cluster visualization

--- exercise6/main.py
+++ exercise6/main.py
@@ -2,28 +2,20 @@ import pandas
 from k_means_clustering import K_Means_Clustering
 from visualize import plot_clusters_2d,show
 
-def parse_axis_values(x_column, y_column, clusters):
-    x_values = []
-    y_values = []
-    for cluster in clusters:
-        x_values += list(cluster[x_column])
-        y_values += list(cluster[y_column])
-    return x_values, y_values
-
-
 data_frame = pandas.read_csv("iris.csv", sep=";")
 df = data_frame.drop('Species', axis=1)
 
 for column in df.columns.to_list():
     df[column] = df[column].str.replace(",", ".").astype(float)
 
-k_means_wrapper = K_Means_Clustering(df, 3)
+k_means_wrapper = K_Means_Clustering(df, 2)
 centroids, clusters = k_means_wrapper.k_means_algorithm()
 
-sepal_x_values, sepal_y_values = parse_axis_values("Sepal.Length", "Sepal.Width", clusters)
-petal_x_values, petal_y_values = parse_axis_values("Petal.Length", "Petal.Width", clusters)
+sepal_centroids = centroids[["Sepal.Length","Sepal.Width"]]
 
-plot_clusters_2d(sepal_x_values, "Sepal Length", sepal_y_values, "Sepal Width")
-plot_clusters_2d(petal_x_values, "Petal Length", petal_y_values, "Petal Width")
+petal_centroids = centroids[["Petal.Length","Petal.Width"]]
+
+plot_clusters_2d(clusters, sepal_centroids, "Sepal.Length", "Sepal.Width")
+plot_clusters_2d(clusters, petal_centroids, "Petal.Length", "Petal.Width")
 
 show()
--- exercise6/visualize.py
+++ exercise6/visualize.py
@@ -1,9 +1,17 @@
+import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib.cm as cm
 
-def plot_clusters_2d(x_values, x_label, y_values, y_label):
-    colors = [(0,0,0)]
+def plot_clusters_2d(clusters, centroids, x_label, y_label):
     plt.figure()
-    plt.scatter(x_values, y_values, c=colors)
+    
+    colors = cm.rainbow(np.linspace(0, 1, len(centroids)))
+    for i in range(0, len(centroids)):
+        plt.plot(centroids.iloc[i][x_label], centroids.iloc[i][y_label], 'kx')
+    
+    for i in range(0,len(clusters)):
+        plt.scatter(clusters[i][x_label], clusters[i] [y_label], c=colors[i])
+
     plt.title('Clusters')
     plt.xlabel(x_label)
     plt.ylabel(y_label)
