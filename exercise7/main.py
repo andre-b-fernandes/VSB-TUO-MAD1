5d2aafebf295a1220a8f22ac6d4288237fb6bb15
Fernando Fernandes <fernandoandre49@gmail.com>
2019-12-02 17:42:07 +0100

    prediction example

--- exercise7/main.py
+++ exercise7/main.py
@@ -9,3 +9,6 @@ naive_bayes.train_algorithm()
 
 accuracy = naive_bayes.test_algorithm()
 print("Algorithm Accuracy : " + str(accuracy) + " %")
+tuple_data = ('Overcast','Hot','Normal', True)
+prediction = naive_bayes.predict(tuple_data)
+print("Prediction for " + str(tuple_data) + " is " + prediction)
